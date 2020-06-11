32ed1adaa07697e05392c5bb18f6ebe7db5640d8
gitmachtl <martin@machtl.com>
2020-06-11 13:34:12 +0200

    Revert "more details step description"
    This reverts commit c7e24e14b870239db43ab48610df0cfd0882d742.

--- cardano/ff-testnet/0x_updateCoreNodeKeysAndRestart.sh
+++ cardano/ff-testnet/0x_updateCoreNodeKeysAndRestart.sh
@@ -26,11 +26,11 @@ if [[ ! $1 == "" ]]; then nodeName=$1; else echo "ERROR - Usage: $0 <PoolNodeNam
 
 
 
-#--  STEP 2  -------------------------------------------------------------
+#--  STEP 2  -----------------------------------------------------------
 # Copy the new files into the ./upload directory and give them fixed names
-#-------------------------------------------------------------------------
+#-----------------------------------------------------------------------
 
-#Get the latest file version number
+#Get the latest version number
 latestKESnumber=$(cat ${nodeName}.kes.counter)
 
 #Copy them to a new filename in the upload folder with fixed names
@@ -42,20 +42,17 @@ file_unlock ./upload/${nodeName}.kes.skey
 file_unlock ./upload/${nodeName}.node.opcert
 file_unlock ./upload/${nodeName}.vrf.skey
 
-cp ./${nodeName}.kes-expire.json ./upload/${nodeName}.kes-expire.json            #Copy latest KES expire information to automate alerts from the coreNode if needed
+cp ./${nodeName}.kes-expire.json ./upload/${nodeName}.kes-expire.json            #Copy latest KES expire information -> to automated alerts from the coreNode
 cp ./${nodeName}.kes-${latestKESnumber}.skey ./upload/${nodeName}.kes.skey       #Copy latest KES key over to fixed name nodeName.kes.skey
 cp ./${nodeName}.node-${latestKESnumber}.opcert ./upload/${nodeName}.node.opcert #Copy latest opcert over to fixed name nodeName.node.opcert
-cp ./${nodeName}.vrf.skey ./upload/${nodeName}.vrf.skey				 #Copy vrf key over to fixed name nodeName.vrf.skey, sure is sure :-)
+cp ./${nodeName}.vrf.skey ./upload/${nodeName}.vrf.skey				 #Copy vrf key over to fixed name nodeName.vrf.skey
 
 
 
 
-#--  STEP 3  --------------------------------------------------------------------
-# Upload the files from the ./upload directory to the coreNode via SCP connection
-#
-# You need to provide your remote connection settings in the 00_common.sh file!!!
-# Make sure you can ssh into your remote server with ssh public/private keypairs
-#--------------------------------------------------------------------------------
+#--  STEP 3  ----------------------------------------------------------
+# Upload the files from the ./upload to the coreNode via SCP connection
+#----------------------------------------------------------------------
 
 echo -e "\e[0mUploading new files now ...\e[90m"
 #Upload them to the CoreNode Server
@@ -67,15 +64,12 @@ echo -e "\e[0mDONE.\e[90m\n"
 
 #--  STEP 4  ---------------------------------------------------------
 # Execute the core restart script on the coreNode to load the new keys
-#
-# This should be automated on the coreNode itself in the future to
-# find a nice leaderslot free spot in the scheduler.
 #---------------------------------------------------------------------
 
 #Executing a coreNode restart on the server
 echo -e "\e[0mInitiating coreNode restart ...\e[90m"
 ssh -p ${remoteServerSSHport} -tq ${remoteServerUser}@${remoteServerAddr} ${remoteServerPostCommand}
-echo -e "\e[0mDONE. coreNode should now run with the new Keys!"
+echo -e "\e[0mDONE. coreNode now running with the new Keys!"
 echo
 
 
