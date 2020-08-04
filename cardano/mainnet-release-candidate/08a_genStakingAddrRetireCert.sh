864c07f6a269c79dd9864b823221bec33c125d73
gitmachtl <martin@machtl.com>
2020-08-04 11:21:02 +0200

    bugfix for mainnet

--- cardano/mainnet-release-candidate/08a_genStakingAddrRetireCert.sh
+++ cardano/mainnet-release-candidate/08a_genStakingAddrRetireCert.sh
@@ -14,13 +14,13 @@ if [[ $# -eq 1 && ! $1 == "" ]]; then addrName=$1; else echo "ERROR - Usage: $0 
 
 #create a stake-address de-registration certificate
 file_unlock ${addrName}.staking.dereg-cert
-${cardanocli} shelley stake-address deregistration-certificate --staking-verification-key-file ${addrName}.staking.vkey --out-file ${addrName}.staking.dereg-cert
+${cardanocli} shelley stake-address deregistration-certificate --stake-verification-key-file ${addrName}.staking.vkey --out-file ${addrName}.staking.dereg-cert
 checkError "$?"
 file_lock ${addrName}.staking.dereg-cert
 
 echo
 echo -e "\e[0mStaking-Address-DeRegistration-Certificate built: \e[32m ${addrName}.staking.dereg-cert \e[90m"
-cat ${addrName}.staking.cert
+cat ${addrName}.staking.dereg-cert
 echo
 echo
 echo -e "\e[35mIf you wanna de-register the Staking-Address, please use script 08b now!\e[0m"
