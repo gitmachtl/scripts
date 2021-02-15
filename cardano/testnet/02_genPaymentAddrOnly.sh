4a2403ac5bc7d2d0229f8083985e7011d7d494e3
gitmachtl <martin@machtl.com>
2021-02-15 20:22:33 +0100

    Added MultiAccounts for Hardware-Wallets

--- cardano/testnet/02_genPaymentAddrOnly.sh
+++ cardano/testnet/02_genPaymentAddrOnly.sh
@@ -12,19 +12,31 @@
 
 #Check command line parameter
 #Check command line parameter
-if [ $# -ne 2 ] || [[ ! ${2^^} =~ ^(CLI|HW)$ ]]; then
+if [ $# -lt 2 ] || [[ ! ${2^^} =~ ^(CLI|HW)$ ]]; then
 cat >&2 <<EOF
-ERROR - Usage: $(basename $0) <AddressName> <KeyType: cli | hw>
+ERROR - Usage: $(basename $0) <AddressName> <KeyType: cli | hw> [Account# 0-1000 for HW-Wallet-Path, Default=0]
 
 Examples:
 $(basename $0) owner cli    ... generates a PaymentOnly Address via cli (was default method before)
 $(basename $0) owner hw     ... generates a PaymentOnly Address by using a Ledger/Trezor HW-Wallet
+
+Optional with Hardware-Account-Numbers:
+$(basename $0) owner hw 1   ... generates a PaymentOnly Address  by using a Leder/Trezor HW-Wallet and SubAccount #1 (Default=0)
+$(basename $0) owner hw 5   ... generates a PaymentOnly Address  by using a Leder/Trezor HW-Wallet and SubAccount #51 (Default=0)
 
 EOF
 exit 1;
 else
 addrName="$(dirname $1)/$(basename $1 .addr)"; addrName=${addrName/#.\//};
 keyType=$2;
+        accountNo=0;
+        if [ $# -eq 3 ]; then
+        accountNo=$3;
+        #Check if the given accountNo is a number and in the range. limit it to 1000 and below. actually the limit is 2^31-1, but thats ridiculous
+	if [ "${accountNo}" == null ] || [ -z "${accountNo##*[!0-9]*}" ] || [ "${accountNo}" -lt 0 ] || [ "${accountNo}" -gt 1000 ]; then echo -e "\e[35mERROR - Account# for the HardwarePath is out of range (0-1000, warnings above 100)!\e[0m"; exit 2; fi
+        fi
+
+
 fi
 
 #warnings
@@ -63,7 +75,7 @@ if [[ ${keyType^^} == "CLI" ]]; then #Building it from the cli
 
         #We need a enterprise paymentonly keypair with vkey and hwsfile from a Hardware-Key, so lets' create them
         start_HwWallet; checkError "$?"; if [ $? -ne 0 ]; then exit $?; fi
-        tmp=$(${cardanohwcli} address key-gen --path 1852H/1815H/0H/0/0 --verification-key-file ${addrName}.vkey --hw-signing-file ${addrName}.hwsfile 2> /dev/stdout)
+        tmp=$(${cardanohwcli} address key-gen --path 1852H/1815H/${accountNo}H/0/0 --verification-key-file ${addrName}.vkey --hw-signing-file ${addrName}.hwsfile 2> /dev/stdout)
         if [[ "${tmp^^}" == *"ERROR"* ]]; then echo -e "\e[35m${tmp}\e[0m\n"; exit 1; else echo -e "\e[32mDONE\e[0m\n"; fi
         checkError "$?"; if [ $? -ne 0 ]; then exit $?; fi
 
@@ -73,10 +85,10 @@ if [[ ${keyType^^} == "CLI" ]]; then #Building it from the cli
         file_lock ${addrName}.vkey
         file_lock ${addrName}.hwsfile
 
-	echo -e "\e[0mPaymentOnly(Enterprise)-Verification-Key: \e[32m ${addrName}.vkey \e[90m"
+	echo -e "\e[0mPaymentOnly(Enterprise)-Verification-Key (Account# ${accountNo}): \e[32m ${addrName}.vkey \e[90m"
         cat ${addrName}.vkey
         echo
-        echo -e "\e[0mPaymentOnly(Enterprise)-HardwareSigning-File: \e[32m ${addrName}.hwsfile \e[90m"
+        echo -e "\e[0mPaymentOnly(Enterprise)-HardwareSigning-File (Account# ${accountNo}): \e[32m ${addrName}.hwsfile \e[90m"
         cat ${addrName}.hwsfile
         echo
 
--- cardano/testnet/03a_genStakingPaymentAddr.sh
+++ cardano/testnet/03a_genStakingPaymentAddr.sh
@@ -11,24 +11,31 @@
 . "$(dirname "$0")"/00_common.sh
 
 #Check command line parameter
-if [ $# -ne 2 ] || [[ ! ${2^^} =~ ^(CLI|HW|HYBRID)$ ]]; then
+if [ $# -lt 2 ] || [[ ! ${2^^} =~ ^(CLI|HW|HYBRID)$ ]]; then
 cat >&2 <<EOF
-ERROR - Usage: $(basename $0) <AddressName> <KeyType: cli | hw | hybrid>
+ERROR - Usage: $(basename $0) <AddressName> <KeyType: cli | hw | hybrid> [Account# 0-1000 for HW-Wallet-Path, Default=0]
 
 Examples:
 $(basename $0) owner cli		... generates Payment & Staking keys via cli (was default method before)
 $(basename $0) owner hw		... generates Payment & Staking keys using Ledger/Trezor HW-Keys
 $(basename $0) owner hybrid	... generates Payment keys using Ledger/Trezor HW-Keys, Staking keys via cli (comfort mode for multiowner pools)
 
+Optional with Hardware-Account-Numbers:
+$(basename $0) owner hw 1       ... generates Payment & Staking keys using Ledger/Trezor HW-Keys and SubAccount #1 (Default=0)
+$(basename $0) owner hybrid 5   ... generates Payment keys using Ledger/Trezor HW-Keys with SubAccount #5, Staking keys via cli (comfort mode for multiowner pools)
+
 EOF
 exit 1;
 else
 addrName="$(dirname $1)/$(basename $1 .addr)"; addrName=${addrName/#.\//};
 keyType=$2;
+	accountNo=0;
+	if [ $# -eq 3 ]; then
+	accountNo=$3;
+	#Check if the given accountNo is a number and in the range. limit it to 1000 and below. actually the limit is 2^31-1, but thats ridiculous
+	if [ "${accountNo}" == null ] || [ -z "${accountNo##*[!0-9]*}" ] || [ "${accountNo}" -lt 0 ] || [ "${accountNo}" -gt 1000 ]; then echo -e "\e[35mERROR - Account# for the HardwarePath is out of range (0-1000, warnings above 100)!\e[0m"; exit 2; fi
+	fi
 fi
-
-
-
 
 #warnings
 if [ -f "${addrName}.payment.vkey" ]; then echo -e "\e[35mWARNING - ${addrName}.payment.vkey already present, delete it or use another name !\e[0m"; exit 2; fi
@@ -58,7 +65,7 @@ if [[ ${keyType^^} == "CLI" ]]; then
 
 	#We need a payment(base) keypair with vkey and hwsfile from a Hardware-Key, sol lets' create them
         start_HwWallet; checkError "$?"; if [ $? -ne 0 ]; then exit $?; fi
-  	tmp=$(${cardanohwcli} address key-gen --path 1852H/1815H/0H/0/0 --verification-key-file ${addrName}.payment.vkey --hw-signing-file ${addrName}.payment.hwsfile 2> /dev/stdout)
+  	tmp=$(${cardanohwcli} address key-gen --path 1852H/1815H/${accountNo}H/0/0 --verification-key-file ${addrName}.payment.vkey --hw-signing-file ${addrName}.payment.hwsfile 2> /dev/stdout)
         if [[ "${tmp^^}" == *"ERROR"* ]]; then echo -e "\e[35m${tmp}\e[0m\n"; exit 1; else echo -e "\e[32mDONE\e[0m\n"; fi
         checkError "$?"; if [ $? -ne 0 ]; then exit $?; fi
 
@@ -68,10 +75,10 @@ if [[ ${keyType^^} == "CLI" ]]; then
 
         file_lock ${addrName}.payment.vkey
         file_lock ${addrName}.payment.hwsfile
-        echo -e "\e[0mPayment(Base)-Verification-Key: \e[32m ${addrName}.payment.vkey \e[90m"
+        echo -e "\e[0mPayment(Base)-Verification-Key (Account# ${accountNo}): \e[32m ${addrName}.payment.vkey \e[90m"
         cat ${addrName}.payment.vkey
         echo
-        echo -e "\e[0mPayment(Base)-HardwareSigning-File: \e[32m ${addrName}.payment.hwsfile \e[90m"
+        echo -e "\e[0mPayment(Base)-HardwareSigning-File (Account# ${accountNo}): \e[32m ${addrName}.payment.hwsfile \e[90m"
         cat ${addrName}.payment.hwsfile
         echo
 
@@ -98,7 +105,7 @@ if [[ ${keyType^^} == "CLI" || ${keyType^^} == "HYBRID" ]]; then
 
         #We need the staking keypair with vkey and hwsfile from a Hardware-Key, sol lets' create them
         start_HwWallet; checkError "$?"; if [ $? -ne 0 ]; then exit $?; fi
-        tmp=$(${cardanohwcli} address key-gen --path 1852H/1815H/0H/2/0 --verification-key-file ${addrName}.staking.vkey --hw-signing-file ${addrName}.staking.hwsfile 2> /dev/stdout)
+        tmp=$(${cardanohwcli} address key-gen --path 1852H/1815H/${accountNo}H/2/0 --verification-key-file ${addrName}.staking.vkey --hw-signing-file ${addrName}.staking.hwsfile 2> /dev/stdout)
         if [[ "${tmp^^}" == *"ERROR"* ]]; then echo -e "\e[35m${tmp}\e[0m\n"; exit 1; else echo -e "\e[32mDONE\e[0m\n"; fi
         checkError "$?"; if [ $? -ne 0 ]; then exit $?; fi
 
@@ -109,10 +116,10 @@ if [[ ${keyType^^} == "CLI" || ${keyType^^} == "HYBRID" ]]; then
         file_lock ${addrName}.staking.vkey
         file_lock ${addrName}.staking.hwsfile
 
-        echo -e "\e[0mVerification(Rewards)-Staking-Key: \e[32m ${addrName}.staking.vkey \e[90m"
+        echo -e "\e[0mVerification(Rewards)-Staking-Key (Account# ${accountNo}): \e[32m ${addrName}.staking.vkey \e[90m"
         cat ${addrName}.staking.vkey
         echo
-        echo -e "\e[0mSigning(Rewards)-HardwareSigning-File: \e[32m ${addrName}.staking.hwsfile \e[90m"
+        echo -e "\e[0mSigning(Rewards)-HardwareSigning-File (Account# ${accountNo}): \e[32m ${addrName}.staking.hwsfile \e[90m"
         cat ${addrName}.staking.hwsfile
         echo
 
