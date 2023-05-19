294c1666cd7a1d2fd7246e20e6d943c34656deea
gitmachtl <martin@machtl.com>
2023-05-19 20:05:52 +0200

    Little fix for the TTL in scripts 11

--- cardano/mainnet/11a_mintAsset.sh
+++ cardano/mainnet/11a_mintAsset.sh
@@ -173,6 +173,7 @@ currentTip=$(get_currentTip);
 
 #set timetolife (inherent hereafter) to the currentTTL or to the value set in the policy.script for the "before" slot (limited policy lifespan)
 ttlFromScript=$(cat ${policyName}.policy.script | jq -r ".scripts[] | select(.type == \"before\") | .slot" 2> /dev/null || echo "unlimited")
+if [[ ${ttlFromScript} == "" ]]; then ttlFromScript="unlimited"; fi #also set it to unlimited if empty
 if [[ ! ${ttlFromScript} == "unlimited" ]]; then ttl=${ttlFromScript}; else ttl=$(get_currentTTL); fi
 echo
 echo -e "\e[0mPolicy valid before Slot-Height:\e[33m ${ttlFromScript}\e[0m"
