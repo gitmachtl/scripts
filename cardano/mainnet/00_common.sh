9915e3488e15f8f6e4b5e9e056f62f6de5b64280
gitmachtl <martin@machtl.com>
2025-11-28 10:30:52 +0100

    et supported cardano-cli maxVersion to 10.12.99
    - There are breaking changes in cardano-cli 10.13.x outputs, which are not yet in these scripts. Stay tuned.

--- cardano/mainnet/00_common.sh
+++ cardano/mainnet/00_common.sh
@@ -328,7 +328,7 @@ if [[ "${magicparam}" == "" || ${addrformat} == "" ||  ${byronToShelleyEpochs} =
 
 #Don't allow to overwrite the needed Versions, so we set it after the overwrite part
 minCliVersion="10.12.0"			#minimum allowed cli version for this script-collection version
-maxCliVersion="10.12.0"  		#maximum allowed cli version, 99.99.9 = no limit so far
+maxCliVersion="10.12.99"  		#maximum allowed cli version, 99.99.9 = no limit so far
 minNodeVersion="10.5.3"  		#minimum allowed node version for this script-collection version
 maxNodeVersion="99.99.9"  		#maximum allowed node version, 99.99.9 = no limit so far
 minLedgerCardanoAppVersion=${ENV_MINLEDGERCARDANOAPPVERSION:-"7.1.4"}  	#minimum version for the cardano-app on the Ledger HW-Wallet
